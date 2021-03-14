60b96bef5cb3627f078650e730a0cb6932e5ea05
atlonrg <atlonrg@gmail.com>
2021-03-14 16:28:56 +0000

    trabajando con includer

---
+++ includes/templates/footer.php
@@ -0,0 +1,70 @@
+
+    <footer class="site-footer">
+        <div class="contenedor clearfix">
+            <div class="footer-informacion">
+                <h3>Sobre <span>gdlwebcamp</span></h3>
+                <p>Praesent rutrum efficitur pharetra. Vivamus scelerisque pretium velit, id tempor turpis pulvinar et.
+                    Ut
+                    bibendum finibus massa non molestie. Curabitur urna metus, placerat gravida lacus ut, lacinia congue
+                    orci.
+                    Maecenas luctus mi at ex
+                    blandit vehicula. Morbi porttitor tempus euismod.</p>
+            </div>
+            <div class="ultimos-tweets">
+                <h3>Últimos <span>tweets</span></h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere minus cum fugiat eos! Totam magni
+                    quidem
+                    doloribus, aut quia exercitationem sunt iure sed. Beatae adipisci quia, omnis fugiat porro iusto
+                    consequuntur
+                    rerum exercitationem!</p>
+
+            </div>
+            <div class="menu">
+                <h3>Redes <span>sociales</span></h3>
+                <nav class="redes-sociales">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter-square"></i></a>
+                    <a href="#"><i class="fab fa-pinterest-square"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </nav>
+            </div>
+        </div>
+
+        <p class="copyright">
+            Todos los derechos Reservados GDLWEBCAMP 2016.
+        </p>
+    </footer>
+
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <script src="js/leaflet.js"></script>
+    <script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-3.4.1.min.js"><\/script>')
+    </script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="jquery.countdown-2.2.0/jquery.countdown.min.js"></script>
+    <script src="waypoints/lib/jquery.waypoints.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="Lettering.js/jquery.lettering.js"></script>
+    <script src="js/main.js"></script>
+
+
+
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+    window.ga = function() {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('set', 'transport', 'beacon');
+    ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
+
+</body>
+
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>GdlWebCamp</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Oswald&family=PT+Sans&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+
+
+    <header class="site-header">
+        <div class="hero">
+            <div class="contenido-header">
+                <nav class="redes-sociales">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-twitter-square"></i></a>
+                    <a href="#"><i class="fab fa-pinterest-square"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </nav>
+                <div class="informacion-evento">
+                    <div class="clearfix">
+                        <p class="fecha"><i class="far fa-calendar-alt"></i> 10/12/Dic</p>
+                        <p class="ciudad"><i class="fas fa-map-marker-alt"></i> Las Palmas</p>
+                    </div>
+                    <!--info evento-->
+                    <h1 class="nombre-sitio">GdlWebCamp</h1>
+                    <p class="eslogan">La mejr conferencia de <span>diseño web</span></p>
+                </div>
+            </div>
+        </div>
+        <!--hero-->
+    </header>
+    <div class="barra">
+        <div class="contenedor clearfix">
+            <div class="logo">
+               <a href="index.php">
+                <img src="img/logo.svg" alt="logo gdlwebcam">
+                </a>
+            </div>
+            <div class="menu-movil">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <nav class="navegacion-principal clearfix">
+                <a href="conferencia.php">Conferencia</a>
+                <a href="#">Calendario</a>
+                <a href="#">Invitados</a>
+                <a href="registro.php">Reservaciones</a>
+            </nav>
+        </div>
+        <!--sierre contenedor-->
+    </div>    <!--sierre de barra navegasion-->
